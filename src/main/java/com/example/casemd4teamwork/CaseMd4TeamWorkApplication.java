9468e67cebe6e14a419c9dc724f6772fa35d27a7
nguyenthanhliem-ntl <nguyenthanhlimpk@gmail.com>
2021-08-04 14:04:18 +0700

    up

---
+++ src/main/java/com/example/casemd4teamwork/CaseMd4TeamWorkApplication.java
@@ -0,0 +1,13 @@
+package com.example.casemd4teamwork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaseMd4TeamWorkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CaseMd4TeamWorkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/casemd4teamwork/CaseMd4TeamWorkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.casemd4teamwork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaseMd4TeamWorkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
