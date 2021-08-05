b67ef945caa79e58dad70be3bfda9b8f15fbadb7
chienpro1605 <leducchien468@gmail.com>
2021-08-05 16:17:22 +0700

    ha

--- src/main/java/com/example/casemd4teamwork/model/Image.java
+++ src/main/java/com/example/casemd4teamwork/model/Image.java
@@ -15,4 +15,35 @@ public class Image {
     @JoinColumn(name = "home_id")
     private Home home;
 
+    public Image() {
+    }
+
+    public Image(String image, Home home) {
+        this.image = image;
+        this.home = home;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public Home getHome() {
+        return home;
+    }
+
+    public void setHome(Home home) {
+        this.home = home;
+    }
 }
